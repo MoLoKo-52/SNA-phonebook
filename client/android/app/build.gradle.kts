95b27f2adb54ea0739b35bfb7090a89b67aa48a1
mc_lavrushka <mc.lavrushka@gmail.com>
2025-05-17 20:29:51 +0300

    feat: add client ci/cd

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
-
+val keystoreProperties = Properties().apply {
+    load(File(rootDir, "key.properties").inputStream())
+}
 android {
     namespace = "com.example.phonebook_interface"
     compileSdk = flutter.compileSdkVersion
@@ -29,13 +31,21 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+    signingConfigs {
+    create("release") {
+        keyAlias = keystoreProperties["keyAlias"] as String
+        keyPassword = keystoreProperties["keyPassword"] as String
+        storeFile = file(keystoreProperties["storeFile"] as String)
+        storePassword = keystoreProperties["storePassword"] as String
+    }
+}
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
+        signingConfig = signingConfigs.getByName("release")
+        isMinifyEnabled = false
+        isShrinkResources = false
+    }
     }
 }
 
